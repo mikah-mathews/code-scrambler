bcad8ddaabc3a9432b493fa576906475f34c50a6
Mikah Mathews <mikah@outofband.org>
2020-03-23 16:27:39 -0700

    Make error with variables, not working correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
-$(button).click(function(){
-    userInput = prompt("Please enter your sentence to be coded:");
+jQuery(document).ready(function() {
+    var userInput = "";
+    var codedSentence = "";
+    jQuery("p").click(function(){
+        userInput = prompt("Please enter your sentence to be coded:");
+    });
+
+
+
+    var capitalizeLetters = function(sentence) {
+        return codedSentence = userInput.slice(0, 2).toUpperCase + userInput.slice(-1).toUpperCase;
+    }
+    console.log(capitalizeLetters);
+
+    var reverseLetters = function(sentence) {
+        return codedSentence = codedSentence.str.split("").reverse().join("");
+    }
+    console.log(reverseLetters);
+
+    var finalCodedMessage = function(sentence) {
+        return userInput.concat(codedSentence);
+    }
+
+    alert(finalCodedMessage);
+    console.log(finalCodedMessage);
+
+    
 });
-
-
-
-sentence();
-
-
-
-var capitalizeLetters = function(sentence) {
-    return codedSentence = userInput.slice(0, 2).toUpperCase + userInput.slice(-1).toUpperCase;
-}
-
-var userInput = "";
-var codedSentence = "";
