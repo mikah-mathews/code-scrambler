c5dcc872520fd7ea135e46df39bad69de1ec0f67
Mikah Mathews <mikah@outofband.org>
2020-03-23 15:27:50 -0700

    Add readme, set up better html

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var origSentence = function(sentence) {
+    console.log(sentence);
+};
+
+var sentence = prompt("Please enter your sentence to be coded:");
+
+
+var firstChar = sentence.charAt(str.length(0) + 1);
+var lastChar = sentence.charAt();
+
+var combinedChar = firstChar.toUpperCase() + lastChar.toUpperCase();
+
+sentence();
+alert(combinedChar);
